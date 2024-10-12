196fae6fee62f6001046ca591dcbd01a5379e6e5
Jesus Rodriguez <jarh1992@gmail.com>
2024-10-11 22:55:36 -0500

    added locations attribute

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,6 @@
 class Store:
-    def __init__(self, name, url, scraper):
+    def __init__(self, name, url, locations, scraper):
         self.name = name
         self.url = url
+        self.locations = locations
         self.scraper = scraper
