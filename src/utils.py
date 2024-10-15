408566b657ac0674511fe96222d0288b4086b19a
Jesus Rodriguez <jarh1992@gmail.com>
2024-10-14 20:43:23 -0500

    added new functions

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,15 @@
-from datetime import datetime
+import re
 
+import numpy as np
+import pandas as pd
 import unicodedata
 from azure.storage.blob import ContentSettings
 from pathlib import Path
 import requests
 import mimetypes
 from urllib.parse import urlparse
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def remove_accents(text):
@@ -13,8 +17,9 @@ def remove_accents(text):
     nfkd_form = unicodedata.normalize('NFD', text)
     # Filtra los caracteres diacríticos (como los acentos)
     text_without_accents = ''.join([char for char in nfkd_form if not unicodedata.combining(char)])
-    # Reemplazar la ñ y Ñ manualmente
-    return text_without_accents.replace('ñ', 'n').replace('Ñ', 'N')
+    text_without_accents = text_without_accents.upper()
+    text_without_accents = text_without_accents.replace('Ñ', 'N')
+    return text_without_accents
 
 
 def upload_blob_file(storage_sas_url: str, dst_folder: str, file_path: Path):
@@ -37,3 +42,43 @@ def upload_blob_file(storage_sas_url: str, dst_folder: str, file_path: Path):
             params={'file': file_path}
         )
     return response.status_code
+
+
+def add_sku(data):
+    hml = pd.read_excel("docs/Homologacion.xlsx", dtype=str)
+    hml['variante'] = hml['variante'].str.upper().replace(",", ".")
+    hml['producto'] = hml['producto'].str.upper()
+    hml['SKU'] = hml['SKU'].astype(str)
+    df = pd.DataFrame(
+        data=[item.split("|") for item in data],
+        columns=["MARCA", "CIUDAD", "POS", "PRODUCTO", "PRECIO"],
+        dtype=str
+    )
+    df['SKU'] = None
+    for i, row in hml.iterrows():
+        trademark = remove_accents(row['trademark'])
+        variant = row['variante'] if row['variante'] is not np.nan else ' '
+        vol = re.search(r"(?<=[(x])\d+\s*ml", row['producto'], re.IGNORECASE)
+        vol = vol.group() if vol else ' '
+        units = re.search(r"(?<=x)\d+und", row['producto'], re.IGNORECASE)
+        units = units.group() if units else ' '
+
+        df['SKU'] = np.where(
+            (df['PRODUCTO'].str.contains(trademark))
+            & (df['PRODUCTO'].str.contains(variant))
+            & (df['PRODUCTO'].str.contains(units))
+            & (df['PRODUCTO'].str.contains(vol)),
+            row['SKU'],
+            df['SKU']
+        )
+    return df
+
+
+def wait_driver(driver, locator: tuple[str, str], timeout=10):
+    WebDriverWait(
+        driver, timeout
+    ).until(
+        ec.presence_of_element_located(
+            locator
+        )
+    )
