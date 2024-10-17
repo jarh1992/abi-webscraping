a7173e1ce401a6d3ddd4f764189471e213a85981
Jesus Rodriguez <jarh1992@gmail.com>
2024-10-17 15:17:52 -0500

    configured scraper with department/city

--- src/scrapers/olimpica.py
+++ src/scrapers/olimpica.py
@@ -1,8 +1,9 @@
 import logging
 import time
 import re
+from urllib.parse import urlparse
 
-from src.utils import remove_accents
+from src.utils import remove_accents, wait_driver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,47 +13,79 @@ from selenium.webdriver.support import expected_conditions as ec
 logger = logging.getLogger(__name__)
 
 
-def scraper(driver, brands, brand_type, url):
-    logger.info(f"Scraping Olimpica {brand_type}")
-    brands = brands[brand_type]
+def scraper(driver, locs, brands, store):
     data = []
-    for coproduct in brands:
+    department, city = list(map(remove_accents, locs[0].split()))
+    department = department.replace('_', ' ')
+    city = city.replace('_', ' ')
+    pos = "NA"
 
-        driver.get(url.format(prod=coproduct))
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
-        driver.execute_script("window.scrollTo(0, 0);")
+    url = urlparse(store.url)
+    scheme_netloc = f"{url.scheme}://{url.netloc}"
+    url_path = url.geturl()
+    driver.get(scheme_netloc)
+    try:
+        pop_up = driver.find_element(By.CLASS_NAME, "olimpica-advance-geolocation-0-x-overlayDirection")
+        change_btn = pop_up.find_elements(By.TAG_NAME, "button")
+        change_btn[1].click()
+    except Exception as e:
+        logger.info('Popup not found')
+        print(e)
+    time.sleep(1)
 
-        driver.execute_script('document.body.style.zoom = 0.55')
-        time.sleep(4)
+    wait_driver(driver, (By.CLASS_NAME, "vtex-modal__overlay"))
+    modal = driver.find_element(By.CLASS_NAME, "vtex-modal__overlay")
+    modal_selects = modal.find_element(By.CLASS_NAME, "olimpica-advance-geolocation-0-x-citiesSelectorContainer")
+    selects = modal_selects.find_elements(By.CLASS_NAME, "olimpica-advance-geolocation-0-x-selectCity")
+    input_dpto = selects[0].find_element(By.TAG_NAME, "input")
+    input_dpto.send_keys(f'{department}\n')
+    wait_driver(modal_selects, (By.CLASS_NAME, "olimpica-advance-geolocation-0-x-selectCity"))
+    selects = modal_selects.find_elements(By.CLASS_NAME, "olimpica-advance-geolocation-0-x-selectCity")
+    input_dpto = selects[1].find_element(By.TAG_NAME, "input")
+    input_dpto.send_keys(f'{city}\n')
 
-        try:
-            WebDriverWait(
-                driver,
-                10
-            ).until(
-                ec.presence_of_element_located((By.XPATH, '//*[@id="gallery-layout-container"]'))
-            )
-            html_content = driver.page_source
-            soup = BeautifulSoup(html_content, 'html.parser')
-            elements = soup.find_all('article', class_=re.compile("vtex-product-summary-2-x-element"))
-            for i in elements:
-                brand = coproduct.replace('Ñ', 'N')
-                description = remove_accents(i.find('span', class_=re.compile("productBrand$")).text.strip()).upper()
-                price = i.find('span', class_=re.compile("olimpica-dinamic-flags-0-x-currencyContainer")).text[2:]
-                row = '|'.join([brand, description, price])
-                if brand_type == 'CERVEZA':
-                    flag = all([i in description for i in [brand_type, *brand.split(' ')]])
-                else:
-                    flag = all([i in description for i in brand.split(' ')])
-                if not flag:
-                    logger.info(f'Product not added: {row}')
-                    continue
-                if row not in data:
-                    data.append(row)
-        except Exception as e:
-            logger.error(f"Error finding element {coproduct}: {e}")
-            continue
+    accept_btn = modal.find_element(By.TAG_NAME, 'button')
+    accept_btn.click()
+    time.sleep(2)
 
-    logger.info(f'Olimpica {brand_type} scraped')
+    for brand_type, brand_lst in brands.items():
+        logger.info(f"Scraping {store.name} {brand_type} in city {city}.")
+        for coproduct in brand_lst:
+            driver.get(url_path.format(prod=coproduct))
+            time.sleep(2)
+
+            try:
+                wait_driver(driver, (By.XPATH, '//*[@id="gallery-layout-container"]'))
+                html_content = driver.page_source
+                soup = BeautifulSoup(html_content, 'html.parser')
+                elements = soup.find_all('article', class_=re.compile("vtex-product-summary-2-x-element"))
+                brand = remove_accents(coproduct)
+                for e in elements:
+                    description = remove_accents(e.find('span', class_=re.compile("productBrand$")).text.strip()).upper()
+                    price = e.find('span', class_=re.compile("olimpica-dinamic-flags-0-x-currencyContainer")).text[2:]
+                    row = '|'.join([brand, city, pos, description, price])
+                    if brand_type == 'CERVEZA':
+                        flag = all([i in description for i in [brand_type, "ML", *brand.split(' ')]])
+                    else:
+                        flag = all([i in description for i in brand.split(' ')])
+                    if not flag:
+                        logger.info(f'Product not added: {brand} != {description}')
+                        continue
+                    if row not in data:
+                        row = re.sub(r'\s+ML', 'ML', row)
+                        row = re.sub(r'X\s+6\s+UND', 'X6UND', row)
+                        row = row.replace('.', ',')
+                        row = row.replace('SIXPACK', 'X6UND')
+                        row = row.replace('SIX PACK', 'X6UND')
+                        row = row.replace('6PACK', 'X6UND')
+                        row = row.replace('6 PACK', 'X6UND')
+                        if ' 1980ML' in row:
+                            row = row.replace('1980ML', '330ML')
+                        row = row.replace(' X 6 ', ' X6UND ')
+                        data.append(row)
+            except Exception as e:
+                logger.error(f"Error finding element {coproduct}: {e}")
+                continue
+
+        logger.info(f'{store.name} {brand_type} scraped')
     return data
