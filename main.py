e04bb435de9552073bf10a37b1442884d747b921
Jesus Rodriguez <jarh1992@gmail.com>
2024-10-17 15:22:35 -0500

    added function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from settings.settings import (
@@ -6,10 +7,10 @@ from settings.settings import (
     dest_folder,
     dest_hist_folder,
     output_folder,
+    STORES,
     BRANDS
 )
-from src.utils import upload_blob_file
-from src.store_info import stores
+from src.utils import upload_blob_file, add_sku
 import argparse
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -17,57 +18,55 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+def run_scraping(driver, st):
+    st_locs = st.locations
+    data = []
+    for locs in st_locs.items():
+        data += st.scraper(driver, locs, BRANDS, st)
+    # test
+    # with open(output_folder / f'{st.name}_products_cp.csv', 'r', encoding='utf8') as file:
+    #     data = file.read().splitlines()
+    data = add_sku(data)
+    data.to_csv(output_folder / DT.strftime(f'{st.name}_products_%Y%m%d.csv'), index=False, sep='|')
+    data.to_csv(output_folder / f'{st.name}_products.csv', index=False, sep='|')
+    logger.info(f"Scraping completed - {st.name} Files created")
+
+
 def main():
-    str_stores = "\n".join([f"- {f}" for f in stores.keys()])
+    str_stores = "\n".join([f"- {f}" for f in STORES.keys()])
     description = f"Run scraper for all stores:\n{str_stores}"
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-st', '--store', type=str, help="Store name", default="all")
     parser.add_argument('-ns', '--not-send', action='store_true', help="Don't send files")
     args = parser.parse_args()
 
-    dt = datetime.now()
     chrome_options = Options()
+    # chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--headless=new")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    file_header = "MARCA|PRODUCTO|PRECIO\n"
-    if args.store == "all":
-        for st in stores.values():
-            data = st.scraper(driver, BRANDS, 'CERVEZA', st.url)
-            data += st.scraper(driver, BRANDS, 'OTROS', st.url)
-            data = '\n'.join(data)
-            file_path = output_folder / f'{st.name}_products.csv'
-            file_hist_path = output_folder / dt.strftime(f'{st.name}_products_%Y%m%d.csv')
-            with (file_path.open(mode='w', encoding='utf8') as file,
-                  file_hist_path.open(mode='w', encoding='utf8') as hist):
-                file.write(file_header)
-                file.write(data)
-                hist.write(file_header)
-                hist.write(data)
 
+    if args.store == "all":
+        logger.info(f"Running scraping process in all stores")
+        for st in STORES.values():
+            run_scraping(driver, st)
+            time.sleep(3)
+    elif args.store in STORES:
+        logger.info(f"Running scraping process for {args.store} store")
+        st = STORES[args.store]
+        run_scraping(driver, st)
     else:
-        logger.info(f"Running scraping process to a single store")
-        if args.store in stores:
-            st = stores[args.store]
-            data = st.scraper(driver, BRANDS, 'CERVEZA', st.url)
-            data += st.scraper(driver, BRANDS, 'OTROS', st.url)
-            data = '\n'.join(data)
-            file_path = output_folder / f'{st.name}_products.csv'
-            file_hist_path = output_folder / dt.strftime(f'{st.name}_products_%Y%m%d.csv')
-            with (file_path.open(mode='w', encoding='utf8') as file,
-                  file_hist_path.open(mode='w', encoding='utf8') as hist):
-                file.write(file_header)
-                file.write(data)
-                hist.write(file_header)
-                hist.write(data)
-        else:
-            logger.error("Invalid store")
+        logger.error("Invalid store")
+        driver.quit()
+        exit(1)
 
     driver.quit()
+
     logger.info("Scraping completed")
 
     if not args.not_send:
+        logger.info("Sending files to cloud")
         for file in output_folder.glob('*products.csv'):
-            hist_file = file.parent / f'{file.stem}_{dt.strftime("%Y%m%d")}{file.suffix}'
+            hist_file = file.parent / f'{file.stem}_{DT.strftime("%Y%m%d")}{file.suffix}'
             logger.info(f"Uploading files: {file.name} - {hist_file.name}")
             r = upload_blob_file(sas_url, dest_hist_folder, hist_file)
             logger.info(f"Upload historic file status: {r}")
@@ -80,4 +79,5 @@ def main():
 
 
 if __name__ == "__main__":
+    DT = datetime.now()
     main()
