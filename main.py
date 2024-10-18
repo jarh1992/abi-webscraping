d5287b94d1744136955776e7e1d7b04de3f5b24e
Jesus Rodriguez <jarh1992@gmail.com>
2024-10-18 17:10:52 -0500

    added and applied functions

--- main.py
+++ main.py
@@ -27,9 +27,24 @@ def run_scraping(driver, st):
     # with open(output_folder / f'{st.name}_products_cp.csv', 'r', encoding='utf8') as file:
     #     data = file.read().splitlines()
     data = add_sku(data)
+    return data
+
+
+def create_file(st, data):
     data.to_csv(output_folder / DT.strftime(f'{st.name}_products_%Y%m%d.csv'), index=False, sep='|')
     data.to_csv(output_folder / f'{st.name}_products.csv', index=False, sep='|')
-    logger.info(f"Scraping completed - {st.name} Files created")
+    logger.info(f"{st.name} Files created")
+
+
+def send_file(st):
+    file = output_folder / f'{st.name}_products.csv'
+    hist_file = output_folder / f'{file.stem}_{DT.strftime("%Y%m%d")}{file.suffix}'
+    logger.info(f"Uploading files: {file.name} - {hist_file.name}")
+    r = upload_blob_file(sas_url, dest_hist_folder, hist_file)
+    logger.info(f"Upload historic file status: {r}")
+    r = upload_blob_file(sas_url, dest_folder, file)
+    logger.info(f"Upload file status: {r}")
+    # 201 status is success.
 
 
 def main():
@@ -38,44 +53,43 @@ def main():
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-st', '--store', type=str, help="Store name", default="all")
     parser.add_argument('-ns', '--not-send', action='store_true', help="Don't send files")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Show browser")
     args = parser.parse_args()
 
     chrome_options = Options()
-    # chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("start-maximized")
+    if not args.verbose:
+        chrome_options.add_argument("--headless=new")
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     if args.store == "all":
         logger.info(f"Running scraping process in all stores")
         for st in STORES.values():
-            run_scraping(driver, st)
-            time.sleep(3)
+            data = run_scraping(driver, st)
+            create_file(st, data)
+            if not args.not_send:
+                send_file(st)
+                logger.info(f"File {st.name} sent")
+            else:
+                logger.info(f"File {st.name} not sent")
     elif args.store in STORES:
         logger.info(f"Running scraping process for {args.store} store")
         st = STORES[args.store]
-        run_scraping(driver, st)
+        data = run_scraping(driver, st)
+        create_file(st, data)
+        if not args.not_send:
+            send_file(st)
+            logger.info(f"File {st.name} sent")
+        else:
+            logger.info(f"File {st.name} not sent")
     else:
         logger.error("Invalid store")
         driver.quit()
         exit(1)
 
     driver.quit()
-
     logger.info("Scraping completed")
-
-    if not args.not_send:
-        logger.info("Sending files to cloud")
-        for file in output_folder.glob('*products.csv'):
-            hist_file = file.parent / f'{file.stem}_{DT.strftime("%Y%m%d")}{file.suffix}'
-            logger.info(f"Uploading files: {file.name} - {hist_file.name}")
-            r = upload_blob_file(sas_url, dest_hist_folder, hist_file)
-            logger.info(f"Upload historic file status: {r}")
-            r = upload_blob_file(sas_url, dest_folder, file)
-            logger.info(f"Upload file status: {r}")
-            # 201 status is success.
-        logger.info(f"Scraping completed - Files sent")
-    else:
-        logger.info(f"Scraping completed - Files not sent")
 
 
 if __name__ == "__main__":
